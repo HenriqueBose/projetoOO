d79513c950a50f704fd85b24df63093c786a24c1
HenriqueBose <38119210+HenriqueBose@users.noreply.github.com>
2018-12-06 19:44:51 -0200

    Revert "Merge branch 'master' of https://github.com/HenriqueBose/projetoOO"
    This reverts commit 30768233d19ac35945a16f2ade948fd42cc948a4, reversing
    changes made to c7faf56e62e3157cff5f98756b64ee77eafc4a57.

--- src/projeto/oo/Formulario.java
+++ src/projeto/oo/Formulario.java
@@ -115,10 +115,9 @@ public class Formulario {
                 isr = new FileReader(arquivo);
             BufferedReader br =new BufferedReader(isr);
             String s =br.readLine();
-            while(!"$".equals(s)){
+            while(s !=null){
                 System.out.println(s);
                 s = br.readLine();
-              
             }
             }catch (IOException ex) {
           Logger.getLogger(Formulario.class.getName()).log(Level.SEVERE, null, ex); 
@@ -126,7 +125,7 @@ public class Formulario {
         }
 
     
-        
+
    
 }
 
--- src/projeto/oo/interfaceGrafica/JF_PerguntaLista.java
+++ src/projeto/oo/interfaceGrafica/JF_PerguntaLista.java
@@ -73,7 +73,6 @@ public class JF_PerguntaLista extends javax.swing.JFrame {
         });
 
 <<<<<<< HEAD
-<<<<<<< HEAD
         textAlternativa1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 textAlternativa1ActionPerformed(evt);
@@ -94,8 +93,6 @@ public class JF_PerguntaLista extends javax.swing.JFrame {
 
 =======
 >>>>>>> 2a59f57fd229cdb9c9254e2d51abe62a77685551
-=======
->>>>>>> 5e0816bae03f1ed8d6afb5484ed7326a91be74ee
         jLabel2.setText("A");
 
         jLabel3.setText("B");
@@ -250,7 +247,6 @@ public class JF_PerguntaLista extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
 <<<<<<< HEAD
-<<<<<<< HEAD
     private void textAlternativa3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textAlternativa3ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_textAlternativa3ActionPerformed
@@ -265,8 +261,6 @@ public class JF_PerguntaLista extends javax.swing.JFrame {
 
 =======
 >>>>>>> 2a59f57fd229cdb9c9254e2d51abe62a77685551
-=======
->>>>>>> 5e0816bae03f1ed8d6afb5484ed7326a91be74ee
     /**
      * @param args the command line arguments
      */
